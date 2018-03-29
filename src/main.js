0d1e3b474d4dabd7db2a06424b8c016d93b700e7
ronishoham <31065981+ronishoham@users.noreply.github.com>
2018-03-29 06:59:24 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,19 @@
+class SarineLoupeImage {
+	constractor()
+	{
+	}
+	
+	    first_init () {
+			return new Promise((resolve, reject) => {
+				resolve()
+        })
+    }
+	
+	    full_init () {
+			return new Promise((resolve, reject) => {
+				resolve()
+        })
+		}
+}
+
+window.SarineLoupeImage = SarineLoupeImage;
